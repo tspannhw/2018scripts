cc8138d899e1d6730839d627345daf306e9f7916
Timothy Spann <tspann@hortonworks.com>
2018-12-28 08:49:20 -0500

    Create testtxtgen.py

---
+++ testtxtgen.py
@@ -0,0 +1,11 @@
+from textgenrnn import textgenrnn
+
+# To Install pip3 install textgenrnn
+
+textgen = textgenrnn()
+textgen.generate()
+
+# Run with python3.6 testtxtgen.py
+
+# Output:
+# [META] Please should paint to a base to me. They should stop them.
